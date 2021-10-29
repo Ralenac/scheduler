cd4b8806a71acad4c946438a3b68ceb0b7d44808
Ralenac <t.ralenac@gmail.com>
2021-10-29 16:37:29 -0400

    Add achange in Application component, combine the state for day, days, and appointments into a state into a single object

--- src/components/Application.js
+++ src/components/Application.js
@@ -64,8 +64,15 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday")
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState ({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
 
   useEffect(() => {
@@ -91,9 +98,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days} 
-            value={day} 
-            onChange={setDay} 
+            days={state.days} 
+            value={state.day} 
+            onChange={setDay}
           />
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
